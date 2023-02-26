c8307e476b45df8688efb65d430563c383165205
Shevchuk Michael <mishashev@icloud.com>
2023-02-26 19:43:03 +0300

    1

---
+++ my-app/src/components/error/ErrorMessage.js
@@ -0,0 +1,11 @@
+
+import img from './386.gif'
+
+
+const ErrorMessage = () => {
+    return(
+        <img style={{diplay: 'block', margin: '0 auto'}}  src={img} alt='error'/>
+    )
+}
+
+export default ErrorMessage;
